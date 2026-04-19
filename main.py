b1216a50a4558865ec8f8317f30028aa689dbda3
Gabriel Almeida <gv.almeida06@gmail.com>
2026-04-19 19:42:11 -0300

    Cleaning the text removing special characters

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 import os
-
-
+import re
+from unidecode import unidecode
 def TextProcessor(path_file):
     txt = []
     with open(path_file, "r", encoding="utf-8") as f:
         for line in f:
             line = line.lower().strip()
             if(line != ""):
-                txt.append(line)
+                txt.append(unidecode(line))
     return txt    
 
 
+def CleanedText(text):
+    cleaned_text = []
+    word_count = 0
+    for line in text:
+        if line != "":
+            words = re.findall(r'\b\w+\b', line)
+            cleaned_text.extend(words)
+    return cleaned_text
+                
+                
 path = os.getcwd()
 text = TextProcessor(path + "/meu_novo_mundo.txt")
 
-print(text)
+clean = CleanedText(text)
+
+
+print(clean)
     
